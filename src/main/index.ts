5cac19f02132b5c9e9026ebc82a2cbcc2d3c837d
39zde <git@39z.de>
2024-08-16 19:54:48 +0200

    remove empty line

--- src/main/index.ts
+++ src/main/index.ts
@@ -146,7 +146,6 @@ app.whenReady().then(() => {
 	});
 });
 
-
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
