3c18b12554e2cd539fd272651449599a3636190d
39zde <git@39z.de>
2024-08-16 19:54:51 +0200

    add regexp to match customer title

--- src/renderer/src/util/func/regex.tsx
+++ src/renderer/src/util/func/regex.tsx
@@ -3,6 +3,10 @@ const TitleRx = new RegExp(
 	'gm'
 );
 
+const title = /(?<title>(?<start>^[hHmMFfDd])(?<afterStart>((?<=[Hh])e?rr?\.?)|((?<=[Ff])ra?u?\.?)|((?<=[Mm])(((iste)?r)|(is)?s)\.?)|((?<=[Dd])(octo)?r\.?)))/gm
+const crown = /(?<doctorTitle>([dD](octo)?r\.?\s{0,3}((([mM]ed\.?)|([jJ]ur\.?)|([Dd]ent\.?))\s{0,3}){0,2}){1,3})/gm
+const diploma = /(?<diploma>(([dD]ipl(oma?)?\.?)(\-?\s{0,2}?((Ing)|(Kf[mr]))\.?)|([BM]\.[ABCEMPST]{1}((omp|hem|ath|ci?|hil|con|ech|cc|rim)\.)?)){0,2}|([mM]ag\.))/gm
+
 
 export const rx = {
 	TitleRx
