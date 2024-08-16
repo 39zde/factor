f8673d8bfe284b408dc75477993ef6c4d0b8f72e
39zde <git@39z.de>
2024-08-16 19:54:59 +0200

    create hashing function (subtle)

---
+++ src/renderer/src/util/func/createHash.ts
@@ -0,0 +1,9 @@
+
+async function createHash(data: ArrayBuffer): Promise<string>{
+	const hash = await crypto.subtle.digest("SHA-1",data)
+	const view = new Uint8Array(hash)
+	const out = Array.from(view)
+	.map((b) => b.toString(16).padStart(2, "0"))
+	.join("");
+	return out
+}
