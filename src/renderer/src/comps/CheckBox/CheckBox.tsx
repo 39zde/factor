b92b32cf0039fb36c9eb8eda297eaec8634f8abd
39zde <git@39z.de>
2024-08-17 21:38:40 +0200

    add CheckBox Comp

---
+++ src/renderer/src/comps/CheckBox/CheckBox.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Check } from "lucide-react";
+import "./CheckBox.css"
+
+export function CheckBox({ticked}:{ticked: boolean}):React.JSX.Element{
+	return(
+		<>
+		<div className="checkBox" style={{
+			background: ticked ? "var(--color-primary)": "none"
+		}}>
+			{
+				ticked ? (<>
+					<Check color="var(--color-light-1)"  size={12} strokeWidth={3}/>
+				</>):(<></>)
+			}
+		</div>
+		</>
+	)
+}
