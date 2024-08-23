9768a5e181236c742615d9f0a6f1ce7614601a1b
39zde <git@39z.de>
2024-08-23 19:24:23 +0200

    remove obsolete type exports in Table.d.ts

--- src/renderer/src/util/types/comps/Table/Table.d.ts
+++ src/renderer/src/util/types/comps/Table/Table.d.ts
@@ -2,11 +2,6 @@ export type { TableBodyDisplayProps } from './TableBodyDisplay';
 export type { TableFootDisplayProps } from './TableFootDisplayProps';
 export type { TableProps } from './TableProps';
 export type {
-	TableHeadProps,
-	TableHeadDisplayProps,
-	SortingHookType,
-} from './TableHeadProps';
-export type {
 	TableContextType,
 	ResizeStyle,
 	TableDispatchAction,
