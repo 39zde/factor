0a2f101be2dd4c059b3ea1e6ac8f6e1ef3064040
39zde <git@39z.de>
2024-08-24 07:58:47 +0200

    init separate files for remaining  DatabaseTypes.d.ts file contents , so it can be deleted

---
+++ src/renderer/src/util/types/database/ArticleTypes.d.ts
@@ -0,0 +1,106 @@
+// export interface ArticleType {
+// 	// unique article identifier
+// 	articleID: string;
+// 	// article Name
+// 	name: string;
+// 	// total article count in ownership
+// 	count: number | 'n/a' | 'N/A';
+// 	// (optional) meters or Pieces or hours of work
+// 	unit?: string;
+// 	// (optional) notes regrading the article
+// 	notes?: string[];
+// 	// value added Tax (VAT) in percent
+// 	VAT: number;
+// 	// price of one Unit of article
+// 	price: PriceType;
+// 	// the last time this article experienced some sort of action
+// 	lastSeen?: Date;
+// 	// the security deposit amount for one unit of article
+// 	securityDeposit?: number;
+// 	// (optional) notes on what to look out for while shipping
+// 	shippingNotes?: string[];
+// 	// (optional) acquisition information
+// 	acquisition?: AcquisitionType[];
+// 	// (optional) category Type
+// 	category?: CategoryType[];
+// 	// (optional) how the price changes, when ordering more than one unit
+// 	bulkDiscount?: StepDiscountType | PercentDiscountType;
+// }
+
+// export interface PriceType {
+// 	withVAT: number;
+// 	noVAT: number;
+// 	unit: 'EUR' | 'DM' | 'USD';
+// }
+
+// export interface AcquisitionType {
+// 	// when the article was obtained
+// 	date: Date;
+// 	// who much of it
+// 	count: number;
+// 	// at what costs
+// 	price: PriceType;
+// 	// (optional) where
+// 	location?: string;
+// 	// (optional) additional notes
+// 	notes?: string[];
+// 	//(optional) some identifier for purchase
+// 	purchaseInvoiceID?: string;
+// }
+
+// export interface LastEncounterType {
+// 	date: Date;
+// 	// (optional) id of corresponding quote
+// 	QuoteID?: string;
+// 	// (optional) id of corresponding invoice
+// 	InvoiceID?: string;
+// 	// (optional) id of corresponding delivery note
+// 	DeliveryID?: string;
+// }
+
+// // ordered list from first:  parent category -> ...sub categories -> article category
+// type CategoryType = Set<string>;
+
+// interface StepDiscountType {
+// 	type: 'step';
+// 	// at what number of items a new price gets applies
+// 	steps: number[];
+// 	prices: Set<number>;
+// 	// (optional) at what price the discount the should be stopped, regardless of count
+// 	minPrice?: number;
+// }
+
+// export interface PercentDiscountType {
+// 	type: 'percent';
+// 	// when to apply a new percentage
+// 	steps: Set<number>;
+// 	// the percentage ( 0 - 100)
+// 	percentage: Set<number>;
+// 	// (optional) at what price the discount the should be stopped, regardless of count
+// 	minPrice?: number;
+// }
+
+// export interface DiscountType {
+// 	amount: number;
+// 	unit: 'EUR' | 'DM' | 'USD' | '%';
+// }
+
+// interface BaseType {
+// 	customerID: string | string[];
+// 	// all article Ids and how many of them
+// 	articleList: ArticleListItem[];
+// 	date: Date;
+// 	addressIDs: {
+// 		// (optional) the address id stored in the CustomerType>addresses[n]>addressID with type "shipping" or "both"
+// 		shipping?: string;
+// 		// (optional) the address id stored in the CustomerType>addresses[n]>addressID with type "billing" or "both"
+// 		billing?: string;
+// 	};
+// }
+
+// export interface ArticleListItem {
+// 	// the article ID
+// 	articleID: string;
+// 	// how many
+// 	count: number;
+// }
---
+++ src/renderer/src/util/types/database/DocumentTypes.d.ts
@@ -0,0 +1,33 @@
+
+
+// export interface QuoteType extends BaseType {
+// 	// unique id for the quote
+// 	quoteID: string;
+// 	// (optional) if the articles are rented when are they going to be returned
+// 	returning?: Date;
+// 	// (optional) time in hours
+// 	duration?: number;
+// }
+
+// interface InvoiceType extends BaseType {
+// 	// unique identifier
+// 	invoiceID: string;
+// 	// total price without discount
+// 	price: PriceType;
+// 	// (optional) total price with discount
+// 	priceWithDiscount?: PriceType;
+// 	// (optional) a discount if there are any to apply
+// 	totalDiscount?: DiscountType;
+// }
+
+// export interface DeliveryType extends BaseType {
+// 	deliveryID: string;
+// }
+
+// export interface ReturneeType extends BaseType {
+// 	returnID: string;
+// 	// the Date and time when the item was returned
+// 	returned: Date;
+// 	// (optional) Notes on the returned Items
+// 	notes?: string[];
+// }
