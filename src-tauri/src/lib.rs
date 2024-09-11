b5b85ba28a34c9719b58d363095cf15dfe77db64
39zde <168634234+39zde@users.noreply.github.com>
2024-09-11 11:44:22 +0000

    Update lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,8 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+	::tauri::Builder::default()
+	.plugin(tauri_plugin_fs::init())
+	.plugin(tauri_plugin_window_state::Builder::default().build())
+	.run(tauri::generate_context!())
+	.expect("error while running tauri application");
 }
