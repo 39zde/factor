b1f311b397a00f8f8d17967aaeba7f4741e814d8
39zde <git@39z.de>
2024-09-12 17:19:53 +0200

    add tauri shell plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
 	.plugin(tauri_plugin_fs::init())
 	.plugin(tauri_plugin_window_state::Builder::default().build())
 	.plugin(tauri_plugin_notification::init())
+	.plugin(tauri_plugin_shell::init())
 	.run(tauri::generate_context!())
 	.expect("error while running tauri application");
 }
