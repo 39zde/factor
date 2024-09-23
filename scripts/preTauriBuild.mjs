7b404ee2d9c00bca6715beb53def00979d518427
39zde <git@39z.de>
2024-09-23 08:11:13 +0200

    add preTauriBuild.mjs

---
+++ scripts/preTauriBuild.mjs
@@ -0,0 +1,18 @@
+import { copyFile } from 'fs/promises';
+
+// relative to root
+const devConfigPath = './src-tauri/conf/tauri.prod.conf.json';
+const devConfigTargetPath = './src-tauri/tauri.conf.json';
+const devHtmlPath = './src/index.prod.html';
+const devHtmlTargetPath = './index.html';
+
+async function main() {
+	try {
+		let promises = [copyFile(devConfigPath, devConfigTargetPath), copyFile(devHtmlPath, devHtmlTargetPath)];
+		await Promise.all(promises);
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+main()
