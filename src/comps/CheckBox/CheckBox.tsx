9436e7266b1109924c27df6b5e8e11aee7659228
39zde <git@39z.de>
2024-09-26 08:31:20 +0200

    add type  CheckBoxProps

--- src/comps/CheckBox/CheckBox.tsx
+++ src/comps/CheckBox/CheckBox.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Check } from 'lucide-react';
 // non-lib imports
 import { solids } from '@app';
+import { CheckBoxProps } from '@type';
 import './CheckBox.css';
 
-export function CheckBox({ ticked }: { ticked: boolean }): React.JSX.Element {
+export function CheckBox({ ticked }: CheckBoxProps): React.JSX.Element {
 	return (
 		<>
 			<div
---
+++ src/types/comps/CheckBox/CheckBox.d.ts
@@ -0,0 +1,1 @@
+export type CheckBoxProps = { ticked: boolean };
