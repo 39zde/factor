43e0e227c6cbde762f94df09244303953a5f282c
39zde <git@39z.de>
2024-09-30 08:16:46 +0200

    update tauri config

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,12 @@
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-	::tauri::Builder::default()
-	.plugin(tauri_plugin_fs::init())
-	.plugin(tauri_plugin_window_state::Builder::default().build())
-	.plugin(tauri_plugin_notification::init())
-	.plugin(tauri_plugin_shell::init())
-	.run(tauri::generate_context!())
-	.expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_notification::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,11 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-	::tauri::Builder::default()
-	.plugin(tauri_plugin_fs::init())
-	.plugin(tauri_plugin_window_state::Builder::default().build())
-	.plugin(tauri_plugin_notification::init())
-	.plugin(tauri_plugin_shell::init())
-	.run(tauri::generate_context!())
-	.expect("error while running tauri application");
+    factor_lib::run()
 }
